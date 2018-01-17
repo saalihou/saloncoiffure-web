3c92b32d2347ef730b94f8f32b00cf358ab3ed23
Saalihou Ndiaye <saalihou@gmail.com>
2018-01-17 16:51:12 +0000

    Finaliser le script de migration sql

--- migrate.sql
+++ migrate.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS sdc;
 USE sdc;
 
-DROP TABLE users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
  id      INT NOT NULL PRIMARY KEY,
@@ -10,6 +10,14 @@ CREATE TABLE users
  tel     VARCHAR(20) NOT NULL ,
  prenoms VARCHAR(200) NOT NULL ,
  nom     VARCHAR(50) NOT NULL ,
- role    VARCHAR(20) NOT NULL ,
- etat    VARCHAR(15) NOT NULL ,
+ role    ENUM('ADMIN') ,
+ etat    ENUM('NON_VERIFIE', 'ACTIF', 'INACTIF') NOT NULL
+);
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+  `data` blob NOT NULL,
+  KEY `ci_sessions_timestamp` (`timestamp`)
 );
