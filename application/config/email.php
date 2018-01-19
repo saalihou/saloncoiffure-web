43ed89fd953c57a9959bd5390e28d3e451369323
Saalihou Ndiaye <saalihou@gmail.com>
2018-01-19 02:00:24 +0000

    Implementer un envoi de mail basique dans la methode inscrire du modele Utilisateur

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.sendgrid.net';
+$config['smtp_user'] = 'apikey';
+$config['smtp_pass'] = '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';
+$config['smtp_port'] = 587;
+$config['smtp_timeout'] = 15;
--- application/models/Utilisateur.php
+++ application/models/Utilisateur.php
@@ -7,6 +7,13 @@ class Utilisateur extends CI_Model {
   }
 
   public function inscrire($prenoms, $nom, $email, $mdp) {
+    $this->load->library('email');
+    $this->email->from('noreply@nmcoiffure.com', 'NM Coiffure');
+    $this->email->to($email);
+    $this->email->subject('Vérification de mail');
+    $this->email->message('Veuillez vérifier votre adresse mail en cliquant sur le lien ci après: ');
+    $this->email->send();
+  
     $this->db->insert('users', [
       'prenoms' => $prenoms,
       'nom' => $nom,
