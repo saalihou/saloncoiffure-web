4c03709c52ea2c66e1fcf6680fbacaa23dc22b78
Saalihou Ndiaye <saalihou@gmail.com>
2018-01-19 04:33:56 +0000

    Corriger le choix du dbdriver dans database.php

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => ENVIRONMENT !== 'production' ? 'root' : '',
 	'password' => ENVIRONMENT !== 'production' ? 'passer' : '',
 	'database' => ENVIRONMENT !== 'production' ? 'sdc' : '',
-	'dbdriver' => ENVIRONMENT !== 'production' ? 'mysqli' : '',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
